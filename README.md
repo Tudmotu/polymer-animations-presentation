897c23ac9381ac57a86df59d0e1b1d2cbe03ffcb
Yotam Bar-On <tudmotu@gmail.com>
2015-01-31 18:58:00 +0200

    rm unecessary files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Polymer Animations Presentation
-This is a polymer component that contains an introductory presentation for
-Polymer animations.
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp         = require('gulp'),
-    connect      = require('gulp-connect');
-
-gulp.task('connect', function () {
-    connect.server({
-        //root: 'index.html',
-        port: 8001
-    });
-});
